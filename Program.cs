d6d8a05a36fded8f20fb04642d33f8489c05b463
Jason Smith <jasonlsmith52@gmail.com>
2017-07-07 15:28:29 -0500

    initial

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<(string product, double amount, int quantity)> transactions = new List<(string, double, int)>();
+            transactions.Add(("potato", 1.01, 4));
+            transactions.Add(("taco", 3.50, 10));
+            transactions.Add(("pizza", 12.51, 1));
+            transactions.Add(("ice cream", 4.00, 4));
+            transactions.Add(("gum", 0.50, 4));
+            int totalItems = 0;
+            double totalSales = 0.0;
+
+            foreach((string product, double amount, int quantity)t in transactions) {
+                totalItems += t.quantity;
+                totalSales += t.amount * (double) t.quantity;
+            }
+
+            Console.WriteLine($"total items: {totalItems}");
+            Console.WriteLine($"total sales: {totalSales}");
+
+        }
+    }
+}
